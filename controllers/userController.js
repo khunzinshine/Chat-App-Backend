b1e8cbda5610f8f5c168bf8bc63aded1ab044e9b
khunzinshine <khunzinshine@gmail.com>
2022-10-08 14:12:45 +0630

    implement user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,15 @@
-const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 module.exports.login = async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     if (!user)
-      return res.json({ msg: "Incorrect Username or Password", status: false });
+      return res.json({ msg: 'Incorrect Username or Password', status: false });
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid)
-      return res.json({ msg: "Incorrect Username or Password", status: false });
+      return res.json({ msg: 'Incorrect Username or Password', status: false });
     delete user.password;
     return res.json({ status: true, user });
   } catch (ex) {
@@ -22,10 +22,10 @@ module.exports.register = async (req, res, next) => {
     const { username, email, password } = req.body;
     const usernameCheck = await User.findOne({ username });
     if (usernameCheck)
-      return res.json({ msg: "Username already used", status: false });
+      return res.json({ msg: 'Username already used', status: false });
     const emailCheck = await User.findOne({ email });
     if (emailCheck)
-      return res.json({ msg: "Email already used", status: false });
+      return res.json({ msg: 'Email already used', status: false });
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       email,
@@ -42,10 +42,10 @@ module.exports.register = async (req, res, next) => {
 module.exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await User.find({ _id: { $ne: req.params.id } }).select([
-      "email",
-      "username",
-      "avatarImage",
-      "_id",
+      'email',
+      'username',
+      'avatarImage',
+      '_id',
     ]);
     return res.json(users);
   } catch (ex) {
@@ -76,7 +76,7 @@ module.exports.setAvatar = async (req, res, next) => {
 
 module.exports.logOut = (req, res, next) => {
   try {
-    if (!req.params.id) return res.json({ msg: "User id is required " });
+    if (!req.params.id) return res.json({ msg: 'User id is required ' });
     onlineUsers.delete(req.params.id);
     return res.status(200).send();
   } catch (ex) {
