54fc0e66fbd1e4f6898d8c8a1c9fca173c0c82f3
khunzinshine <khunzinshine@gmail.com>
2022-10-25 15:26:50 +0630

    modify server cors

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://khun-chat-app.vercel.app'],
+    origin: 'https://khun-chat-app.vercel.app',
     credentials: true,
   },
 });
