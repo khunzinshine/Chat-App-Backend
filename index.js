8b9d178df6c0a1be3e77fcf010e5ad533b922efc
khunzinshine <khunzinshine@gmail.com>
2022-10-28 15:31:54 +0630

    modify cors origin

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const socket = require('socket.io');
 require('dotenv').config();
 
+var path = require('path');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,20 +25,15 @@ mongoose
     console.log(err.message);
   });
 
-app.use(
-  cors({
-    origin: 'https://khun-chat-app.vercel.app',
-  })
-);
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 const server = app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+  console.log(`Server started on ${process.env.PORT || 5000}`)
 );
 const io = socket(server, {
   cors: {
-    origin: 'https://khun-chat-app.vercel.app',
+    origin: 'http://localhost:5000',
     credentials: true,
   },
 });
